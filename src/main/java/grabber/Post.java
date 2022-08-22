1678797faf75551b3654b820386c431075d6c7a5
pavel.chupin <pavelchupin94@yandex.ru>
2022-08-22 10:14:57 +0300

    2.2. Модель данных - Post. [#285211]

--- src/main/java/grabber/Post.java
+++ src/main/java/grabber/Post.java
@@ -40,8 +40,12 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
         return Objects.equals(link, post.link);
     }
