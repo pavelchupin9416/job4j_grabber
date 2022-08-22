c0d91d11ac947de2fa0b9fd90ef5d24e206118ed
pavel.chupin <pavelchupin94@yandex.ru>
2022-08-22 10:05:34 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/grabber/Post.java
@@ -0,0 +1,64 @@
+package grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private  int id;
+    private  String title;
+    private  String link;
+    private  String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
