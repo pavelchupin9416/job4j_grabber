e918a0359adaa9fc80ebea988ca964bf2d710d2a
pavel.chupin <pavelchupin94@yandex.ru>
2022-08-23 09:50:47 +0300

    Фикс 2.4. HabrCareerParse [#285213]

--- src/main/java/grabber/HabrCareerParse.java
+++ src/main/java/grabber/HabrCareerParse.java
@@ -24,40 +24,50 @@ public class HabrCareerParse implements Parse {
     }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         HabrCareerParse habrCareerParse = new HabrCareerParse(new HabrCareerDateTimeParser());
         habrCareerParse.list(PAGE_LINK).forEach(System.out::println);
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
         for (int index = 1; index <= PAGES; index++)  {
             Connection connection = Jsoup.connect(link + index);
-            Document document = connection.get();
+            Document document;
+            try {
+                document = connection.get();
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e);
+            }
             Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element dateElement = row.select(".vacancy-card__date").first();
-                Element datelinkElement = dateElement.child(0);
-                LocalDateTime date = dateTimeParser.parse(datelinkElement.attr("datetime"));
-                String vacancyName = titleElement.text();
-                String linkVacancy = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String description = null;
-                try {
-                    description = retrieveDescription(linkVacancy);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                posts.add(new Post(vacancyName, linkVacancy, description, date));
-            }); }
+            for (Element row : rows) {
+                posts.add(getPost(row));
+            }
+        }
         return posts;
     }
 
-        private String retrieveDescription(String link) throws IOException {
+    private Post getPost(Element row) {
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        Element dateElement = row.select(".vacancy-card__date").first();
+        Element datelinkElement = dateElement.child(0);
+        LocalDateTime date = dateTimeParser.parse(datelinkElement.attr("datetime"));
+        String vacancyName = titleElement.text();
+        String linkVacancy = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        String description = retrieveDescription(linkVacancy);
+        return new Post(vacancyName, linkVacancy, description, date);
+    }
+
+    private String retrieveDescription(String link) {
         Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
+        Document document;
+        try {
+            document = connection.get();
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
         Elements element =   document.select(".style-ugc");
         return element.text();
     }
