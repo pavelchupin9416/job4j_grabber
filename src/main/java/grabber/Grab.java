eb3205e4c09669777497d67cc1bdc01f40bcd67c
pavel.chupin <pavelchupin94@yandex.ru>
2022-08-23 09:59:50 +0300

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

---
+++ src/main/java/grabber/Grab.java
@@ -0,0 +1,9 @@
+package grabber;
+
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+public interface Grab {
+    void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException;
+}
